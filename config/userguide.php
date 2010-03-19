42a6fa258b0490aee1acfabde1eda8080f469f19
bluehawk <phroughy@gmail.com>
2010-03-18 22:12:24 -0600

    Changed to work with recent changes to my userguide

---
+++ config/userguide.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'userguide' => array(
+		
+		// This should be the path to your userguide pages
+		'kohana301' => array(
+			
+			// The name that should show up on the userguide index page
+			'name'  => 'Kohana 301',
+			
+			// A short description of this module
+			'desc'  => 'A beginners guide to Kohana 3.',
+			
+			// Where is the menu page for this page? WITHOUT 'guide/' at the beggining or '.md' at the end
+			'menu'  => 'kohana301/menu',
+		)	
+	)
+);
